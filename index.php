093eb1cc443a0a688d3d015e043913fed289eb06
ambs <douglasamber3@gmail.com>
2015-05-11 11:49:51 -0700

    MILESTONE 5

--- index.php
+++ index.php
@@ -1,9 +1,7 @@
 <?php
-
 //Configuration for our PHP Server
 set_time_limit(0);
 ini_set('default_socket_timeout', 300);
-
 session_start();
 
 //Make Constant using define.
@@ -12,16 +10,34 @@ define('clientSecret', '0baa6ac1ab58432a8a288e8c956f7e7a');
 define('redirectURI', 'http://localhost/Amberapi/index.php');
 define('ImageDirectory', 'pics/');
 
+if isset(($_GET['code'])){
+	$code = ($_GET['code']);
+	$url = 'https://api.instagram.com/oauth/access_token';
+	$access_token_settings = array('client_id' => clientID,
+									'client_secret' => clientSecret,
+									'grant_type' => 'authorization_code',
+									'redirect_uri' => redirectURI,
+									'code' => $code
+									);	
+}
+
 ?>
 
 <!DOCTYPE html>
 <html>
-<head>
-	<title></title>
-</head>
-<body>
-	
-	<a href="https://api.instagram.com/oauth/authorize/?client_id=<?php echo clientID; ?>&redirect_uri=<?php echo redirectURI; ?>&response_type=code">Login</a>
-	
-</body>
+	<head>
+		<meta charset="utf-8">
+		<meta name="description" content="">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Untitled</title>
+		<link rel="stylesheet" href="css/style.css">
+		<link rel="author" href="humans.txt">
+	</head>
+	<body>
+		<!--creating a login for people to go to Instagram API
+		creating a link to instagram through oauth/authorizing the account
+		after setting client_id to blank in the begining, along with redirect_uri then you have to echo it out from the constants.-->
+		<a href="https://api.instagram.com/oauth/authorize/?client_id=<?php echo clientID; ?>&redirect_uri=<?php echo redirectURI; ?>&response_type=code">Login</a>
+		<script type="js/main.js"></script>
+	</body>
 </html>
