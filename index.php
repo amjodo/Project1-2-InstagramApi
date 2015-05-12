d2ed75857cec6d742561e8acf005caeecc19309a
ambs <douglasamber3@gmail.com>
2015-05-11 19:10:46 -0700

    MILESTONE 9
    (had an error after milestone 8:
    Warning: curl_close() expects exactly 1 parameter, 0 given in C:\xampp\htdocs\Amberapi\index.php on line 31
    am.bamb)

--- index.php
+++ index.php
@@ -10,6 +10,20 @@ define('clientSecret', '0baa6ac1ab58432a8a288e8c956f7e7a');
 define('redirectURI', 'http://localhost/Amberapi/index.php');
 define('ImageDirectory', 'pics/');
 
+//function that is going to connect to Instagram.
+function connectToInstagram($url){
+	$ch = curl_init();
+
+	curl_setopt_array($ch, array){
+		CURLOPT_URL => $url,
+		CURLOPT_RETURNTRANSFER => true;
+		CURLOPT_SSL_VERIFYPEER => false;
+		CURLOPT_SSL_VERIFYHOST => 2,
+	});
+	$result = curl_exec($ch);
+	curl_close($ch);
+	return $result;
+}
 if (isset($_GET['code'])){
 	$code = ($_GET['code']);
 	$url = 'https://api.instagram.com/oauth/access_token';
